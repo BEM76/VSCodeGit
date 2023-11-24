478c52a762dfca244c0306b8cc2b61503017c7c0
Ben Morter <bmorter@rdt.co.uk>
2023-11-24 14:08:42 +0000

    Undo all

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("dd");
+
 
 Console.WriteLine("dd1");
 Console.WriteLine("NewFeatures12");
@@ -8,3 +8,7 @@ Console.WriteLine("NewFeatures12");
 Console.WriteLine("dd1");
 Console.WriteLine("NewFeatures12");
 Console.WriteLine("NewFeatures12");
+
+Console.WriteLine("1st");
+Console.WriteLine("2nd");
+Console.WriteLine("3rd");
