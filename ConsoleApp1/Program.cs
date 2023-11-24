128bff5b05e2820be59162ad6c0b541da7a3bfbb
Ben Morter <bmorter@rdt.co.uk>
2023-11-24 14:02:15 +0000

    amend

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,3 +3,6 @@ Console.WriteLine("dd");
 
 Console.WriteLine("dd1");
 Console.WriteLine("NewFeatures12");
+Console.WriteLine("dd1");
+Console.WriteLine("NewFeatures12");
+Console.WriteLine("NewFeatures12");
